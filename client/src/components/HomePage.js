581202976ce6d46d8a40c3dc2ff7333c7d04cea0
John Rooney <rooneyjohn@gmail.com>
2023-03-28 15:56:34 -0400

    finished splash page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -14,18 +14,20 @@ function HomePage({ handleLogIn }) {
         <button className='logInButton newbutton' onClick={handleLogIn}> Log IN </button>
         <button className='logInButton newbutton' onClick={handleSignUp}> Sign Up </button>
         <div>
-          <img className='homeImage3' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/riding-raptor.png'/>
-          <img className='homeImage1' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/Bridge-troll.png'/>
-          <img className='homeImage2' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/forest-crusher.png'/>
-          <img className='homeImage4' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/Fire%20Oracle.png'/>
+          <img className='homeImage3' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/ghoul-hunter.png'/>
+          <img className='homeImage1' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/nature-protector.png'/>
+          <img className='homeImage2' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/defender.png'/>
+          <img className='homeImage4' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/dwarvenWarlock.png'/>
           <img className='homeImage5' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/barbarian-raider.png'/>
+          <img className='homeImage6' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/cintar%20warrior.png'/>
         </div>
-        
+        {/* https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/cintar%20warrior.png */}
         <img className='homeImage1b flipped' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/Flame%20warrior.png'/>
         <img className='homeImage2b flipped' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/orc1.png'/>
         <img className='homeImage3b flipped' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/unholy-priest.png'/>
         <img className='homeImage4b flipped' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/demonic-bloodsucker.png'/>
         <img className='homeImage5b flipped' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/undead%20caster.png'/>
+        <img className='homeImage6b flipped' src='https://raw.githubusercontent.com/Irishwolf13/monsterImages/main/monsters/orc-treasure-hunt.png'/>
         
       </div>
     </>
